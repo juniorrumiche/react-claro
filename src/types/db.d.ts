d833c6cd69bddf71f158f2c7cee240aafc98e0a7
Junior Rumiche <juniorrumichem@gmail.com>
2023-05-23 16:11:20 -0500

    I add types for the clear plans

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -1,9 +1,9 @@
-interface SelectInputType {
+export interface SelectInputType {
   value: string;
   text: string;
 }
 
-interface PlanesNetflixDBType {
+export interface PlanesNetflixDBType {
   mb_standar: number;
   nombre_plan: string;
   mb_fullclaro: number;
@@ -13,4 +13,21 @@ interface PlanesNetflixDBType {
   precio_total: number;
 }
 
-interface Planes1PlayDBType extends SelectInputType {}
+export interface PlanesBaseType {
+  nombre_plan: string;
+  velocidad: number;
+  duracion_promo: number;
+  precio_promo: number;
+  precio_real: number;
+  mb_standar: number;
+  mb_fullclaro: number;
+}
+
+export interface Planes1PlayDBType extends SelectInputType {}
+
+export interface Planes2PlaySimpleDBType extends PlanesBaseType {}
+export interface Planes2PlayAvanzadoDBType extends PlanesBaseType {}
+export interface Planes2PlaySuperiorDBType extends PlanesBaseType {}
+
+export interface Planes3PlayAvanzadoDBType extends PlanesBaseType {}
+export interface Planes3PlaySuperiorDBType extends PlanesBaseType {}
